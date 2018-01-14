50f7c9d0f7bac5ef0f485c6f8cc27799bcc88ff4
dglencross <dglencross@gmail.com>
2018-01-14 17:49:35 +0000

    Solve part 1

--- AdventOfCode/Day 21/src/Day21.py
+++ AdventOfCode/Day 21/src/Day21.py
@@ -26,18 +26,22 @@ def combine_n_arrays(arrays):
         line = []
         while subcount < size:
             if len(line) == 0:
+                if arrays[count] is None:
+                    return
                 line = list(arrays[count])
             else:
 
                 for a in line:
-                    for x in a:
-                        if x != '#' and x != '.':
-                            a.remove(x)
+                    if isinstance(a, (list,)):
+                        for x in a:
+                            if x != '#' and x != '.':
+                                a.remove(x)
 
                 for a in arrays[count]:
-                    for x in a:
-                        if x != '#' and x != '.':
-                            a.remove(x)
+                    if isinstance(a, (list,)):
+                        for x in a:
+                            if x != '#' and x != '.':
+                                a.remove(x)
                 line = combine_two_arrays_along_side(line, list(arrays[count]))
             subcount += 1
             count += 1
@@ -55,27 +59,48 @@ def combine_n_arrays(arrays):
 
 
 def matches_rule(array, rule):
+
     if np.array_equal(array, rule):
         return True
 
     flip_horizontal = np.fliplr(rule)
-    if np.array_equal(array, flip_horizontal):
+    if np.array_equal(array, flip_horizontal.tolist()):
+        return True
+
+    flip_horizontal = np.rot90(flip_horizontal)
+    if np.array_equal(array, flip_horizontal.tolist()):
+        return True
+
+    flip_horizontal = np.rot90(flip_horizontal)
+    
+    if np.array_equal(array, flip_horizontal.tolist()):
+        return True
+
+    flip_horizontal = np.rot90(flip_horizontal)
+    if np.array_equal(array, flip_horizontal.tolist()):
         return True
 
     flip_vertical = np.flipud(rule)
-    if np.array_equal(array, flip_vertical):
+    if np.array_equal(array, flip_vertical.tolist()):
         return True
+    flip_vertical = np.rot90(flip_vertical)
 
+    if np.array_equal(array, flip_vertical.tolist()):
+        return True
+    flip_vertical = np.rot90(flip_vertical)
+    if np.array_equal(array, flip_vertical.tolist()):
+        return True
+    flip_vertical = np.rot90(flip_vertical)
+    if np.array_equal(array, flip_vertical.tolist()):
+        return True
     rotated_rule = np.rot90(rule)
-    if np.array_equal(array, rotated_rule):
+    if np.array_equal(array, rotated_rule.tolist()):
         return True
-
     rotated_rule = np.rot90(rotated_rule)
-    if np.array_equal(array, rotated_rule):
+    if np.array_equal(array, rotated_rule.tolist()):
         return True
-
     rotated_rule = np.rot90(rotated_rule)
-    if np.array_equal(array, rotated_rule):
+    if np.array_equal(array, rotated_rule.tolist()):
         return True
 
     return False
@@ -99,37 +124,54 @@ def expand_once(rules, array):
 
 
 def expand(rules, array, iterations):
+
+    print 'iterations remaining: ' + str(iterations)
+
     for x in range(0, iterations):
         if len(array) == 2 or len(array) == 3:
             array = expand_once(rules, array)
         elif len(array) % 2 == 0:
-            splits = split_into_arrays(array)
+            splits = split_into_arrays_of_two(array)
             for i in range(0, len(splits)):
-                # for j in range(0, len(splits[i])):
-                #     splits[i][j] = '\n'.join(splits[i][j])
-
-                if not (splits[i] is None):
-                    splits[i] = np.array(expand(rules, splits[i], iterations - x))
+                result = expand_once(rules, splits[i])
+                splits[i] = result
             array = combine_n_arrays(splits)
-        else:
-            print 3
-        x += 1
+        elif len(array) % 3 == 0:
+            splits = split_into_arrays_of_three(array)
+            for i in range(0, len(splits)):
+                result = expand_once(rules, splits[i])
+                splits[i] = result
+            array = combine_n_arrays(splits)
 
+    # print str(array) + '\n'
     return array
 
 
 def get_expansion(lines, array):
+
     for line in lines:
         line = line.replace(" ", "")
         parts = line.split("=>")
 
-        if matches_rule(array, translate_to_array(parts[0])):
+        for a in array:
+            for x in a:
+                if x != '#' and x != '.':
+                    a.remove(x)
+
+        if isinstance(array, (list,)):
+            array = np.array(array)
+        if matches_rule(array.tolist(), translate_to_array(parts[0])):
             return translate_to_array(parts[1])
 
-    return None
+    raise ValueError('Failed to find a matching rule, this cannot happen')
 
 
 def real_rules():
+    with open("Day21.txt") as textFile:
+        grid = [line for line in textFile]
+    return grid
+
+def single_rules():
     with open("Day21.txt") as textFile:
         grid = [line for line in textFile]
     return grid
@@ -152,7 +194,7 @@ def count_hashes(array):
     return count
 
 
-def split_into_arrays(array):
+def split_into_arrays_of_two(array):
     x = 0
     y = 0
     result = []
@@ -167,6 +209,22 @@ def split_into_arrays(array):
 
     return result
 
+def split_into_arrays_of_three(array):
+    x = 0
+    y = 0
+    result = []
+
+    while y < len(array):
+        while x < len(array):
+            subarray = np.array([[array[y][x], array[y][x + 1], array[y][x + 2]],
+                                 [array[y + 1][x], array[y + 1][x + 1], array[y + 1][x + 2]],
+                                 [array[y + 2][x], array[y + 2][x + 1], array[y + 2][x + 2]]])
+            result.append(subarray)
+            x += 3
+        x = 0
+        y += 3
+
+    return result
 
 class Test_1(unittest.TestCase):
     def test_expand_three_once(self):
@@ -269,10 +327,8 @@ class Test_1(unittest.TestCase):
         self.assertEquals('.', result[5][4])
         self.assertEquals('.', result[5][5])
 
-
-
-        def test_count_after_two_iterations(self):
-            start_array = np.array([['.', '#', '.'], ['.', '.', '#'], ['#', '#', '#']])
+    def test_count_after_two_iterations(self):
+        start_array = np.array([['.', '#', '.'], ['.', '.', '#'], ['#', '#', '#']])
 
         rule_lines = dummy_rules()
 
@@ -280,18 +336,102 @@ class Test_1(unittest.TestCase):
 
         self.assertEquals(12, count_hashes(result))
 
-    def test_count_after_five_iterations(self):
+    # 0 pre: 3
+    # 0 post: 4
+    # 1 pre: 4
+    # 1 post: 6
+    # 2 pre: 6
+    # 2 post: 9
+    # 3 pre: 9
+    # 3 post: 12
+    # 4 pre: 12
+    # 4 post: 18
+
+
+    def test_size_after_one_iteration_dummy(self):
         start_array = np.array([['.', '#', '.'], ['.', '.', '#'], ['#', '#', '#']])
+
         rule_lines = dummy_rules()
+
+        result = expand(rule_lines, start_array, 1)
+
+        print str(result)
+        self.assertEquals(4, len(result))
+
+    def test_size_after_one_iteration(self):
+        start_array = np.array([['.', '#', '.'], ['.', '.', '#'], ['#', '#', '#']])
+
+        rule_lines = real_rules()
+
+        result = expand(rule_lines, start_array, 1)
+
+        print str(result)
+        self.assertEquals(4, len(result))
+
+    def test_size_after_two_iterations(self):
+        start_array = np.array([['.', '#', '.'], ['.', '.', '#'], ['#', '#', '#']])
+
+        rule_lines = real_rules()
+
+        result = expand(rule_lines, start_array, 2)
+
+        print str(result)
+        self.assertEquals(6, len(result))
+
+    def test_size_after_three_iterations(self):
+        start_array = np.array([['.', '#', '.'], ['.', '.', '#'], ['#', '#', '#']])
+
+        rule_lines = real_rules()
+
+        result = expand(rule_lines, start_array, 3)
+
+        self.assertEquals(9, len(result))
+
+    def test_size_after_four_iterations(self):
+        start_array = np.array([['.', '#', '.'], ['.', '.', '#'], ['#', '#', '#']])
+
+        rule_lines = real_rules()
+
+        result = expand(rule_lines, start_array, 4)
+
+        self.assertEquals(12, len(result))
+
+    def test_size_after_five_iterations(self):
+        start_array = np.array([['.', '#', '.'], ['.', '.', '#'], ['#', '#', '#']])
+
+        rule_lines = real_rules()
 
         result = expand(rule_lines, start_array, 5)
 
-        self.assertEquals(12, count_hashes(result))
+        self.assertEquals(18, len(result))
+
+    def test_count_after_five_iterations(self):
+
+        start_array = np.array([['.', '#', '.'], ['.', '.', '#'], ['#', '#', '#']])
+        rule_lines = single_rules()
+
+        result = expand(rule_lines, start_array, 5)
+
+        print len(result)
+        print len(result[0])
+
+        self.assertEquals(142, count_hashes(result))
+
+    def test_count_after_eighteen_iterations(self):
+        start_array = np.array([['.', '#', '.'], ['.', '.', '#'], ['#', '#', '#']])
+        rule_lines = single_rules()
+
+        result = expand(rule_lines, start_array, 18)
+
+        print len(result)
+        print len(result[0])
+
+        self.assertEquals(142, count_hashes(result))
 
     def test_split_4_by_3(self):
         array = np.array([['#', '.', '.', '#'], ['.', '.', '.', '.'], ['.', '.', '.', '.'], ['#', '.', '.', '#']])
 
-        arrays = split_into_arrays(array)
+        arrays = split_into_arrays_of_two(array)
 
         self.assertEquals('#', arrays[0][0][0])
         self.assertEquals('.', arrays[0][0][1])
@@ -316,7 +456,7 @@ class Test_1(unittest.TestCase):
     def test_split_4(self):
         array = np.array([['#', '.', '.', '#'], ['.', '.', '.', '.'], ['.', '.', '.', '.'], ['#', '.', '.', '#']])
 
-        arrays = split_into_arrays(array)
+        arrays = split_into_arrays_of_two(array)
 
         self.assertEquals('#', arrays[0][0][0])
         self.assertEquals('.', arrays[0][0][1])
